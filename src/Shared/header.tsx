b73e54a5d76bf5b4032041f87178aef6d3ef023a
[Mixtrix] <[jefferson024@hotmail.com]>
2026-06-16 22:43:51 -0500

    se creo la estructura basica del componente header

---
+++ src/Shared/header.tsx
@@ -0,0 +1,16 @@
+
+function Header() {
+
+  return (
+    <>
+       <div className="Header">
+        <a className="Logo" href="main.tsx">
+          <img src="logo.png"></img>
+        </a>
+        <p>Hol soy el header xd</p>
+       </div>
+    </>
+  )
+}
+
+export default Header
