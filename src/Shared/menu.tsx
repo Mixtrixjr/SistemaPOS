f3e9919d771ac233201ddbfad25dea8e2642eb11
[Mixtrix] <[jefferson024@hotmail.com]>
2026-06-16 22:44:12 -0500

    se creo la estructura basica del menu lateral

---
+++ src/Shared/menu.tsx
@@ -0,0 +1,29 @@
+
+function Menu() {
+
+  return (
+    <>
+      <div className="Lateral-Menu">
+        <h3>Hola soy el menu lateral que no estoy lateral xd</h3>
+        <div className="Inventario">
+            <a href="inventario.tsx">Inventario</a>
+        </div>
+         <div className="Ventas">
+            <a href="inventario.tsx">Ventas</a>
+        </div>
+         <div className="Pedidos">
+            <a href="inventario.tsx">Pedidos</a>
+        </div>
+         <div className="Usuarios">
+            <a href="inventario.tsx">Usuarios</a>
+        </div>
+         <div className="Informes">
+            <a href="inventario.tsx">Informes</a>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Menu
